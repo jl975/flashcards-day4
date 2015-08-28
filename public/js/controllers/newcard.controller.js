ff27b7d9d6114aebe1f035c5920ab83f1ba5fe21
jl975 <jlee6891@gmail.com>
2015-08-28 15:44:53 -0400

    complete minus extra credit

--- public/js/controllers/newcard.controller.js
+++ public/js/controllers/newcard.controller.js
@@ -2,15 +2,7 @@ app.controller('NewCardController', function ($scope, FlashCardsFactory) {
 
 	$scope.categories = FlashCardsFactory.categories;
 
-    $scope.newCard = {
-    	question: null,
-    	category: null,
-    	answers: [
-	    	{ text: null, correct: false },
-	    	{ text: null, correct: false },
-	    	{ text: null, correct: false }
-    	]
-    }
+
 
     $scope.addCard = function (newCard) {
       FlashCardsFactory.addFlashCard(newCard).then(function(card){
@@ -18,35 +10,18 @@ app.controller('NewCardController', function ($scope, FlashCardsFactory) {
      });
     };
 
-    // $scope.setQuestion = function(text) {
-    // 	$scope.newCard.question = text;
-    // 	return $scope.newCard.question;
-    // }
+    $scope.reset = function() {
+        $scope.newCard = {
+            question: null,
+            category: null,
+            answers: [
+                { text: null, correct: false },
+                { text: null, correct: false },
+                { text: null, correct: false }
+            ]
+        }
+    }
 
-    // $scope.setCategory = function(text) {
-    // 	$scope.newCard.category = text;
-    // 	return $scope.newCard.category;
-    // }
-    
-    // $scope.setAnswer = function(text, i) {
-    // 	$scope.newCard.answers[i].text = text;
-    // }
-    // $scope.setCorrect = function(check, i) {
-    // 	$scope.newCard.answers[i].correct = check ? true : false;
-    // }
-
-    // $scope.sendCard = function(newCard) {
-    //     $.ajax({
-    //       type: "POST",
-    //       url: '/cards',
-    //       data: {
-    //         question: newCard.question,
-    //         category: newCard.category,
-    //         answers: newCard.answers
-    //       },
-    //       success: function() {console.log("success")},
-    //       error: function(err) { throw err;}
-    //     });
-    // }
+    $scope.reset();
 
 });
